18b9d831212b8f38378ddbde68c44dd57d34f29d
R1b0flav1n <crdickson97@gmail.com>
2021-02-08 18:37:34 -0500

    Update footer.php

--- footer.php
+++ footer.php
@@ -10,9 +10,9 @@
     <div id="footer">
         <a href="https://validator.w3.org/check?uri=referer"><img src="http://www.w3.org/Icons/valid-xhtml10" alt="Valid XHTML 1.0!" height="31" width="88" /></a>
         <?php
-        filemtime("home.php");
+        filemtime("index.php");
         echo "<br />";
-        echo "Last modified: ".date("F d Y H:i:s.", filemtime("home.php"));
+        echo "Last modified: ".date("F d Y H:i:s.", filemtime("index.php"));
         if (!isset($_SESSION['user'])) {
         echo " User not logged in.";
         } else echo " User: {$_SESSION['user']}";
