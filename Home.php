529c258678490dd6a11033f71d7f88b4b5c0db38
R1b0flav1n <crdickson97@gmail.com>
2021-02-08 17:39:13 -0500

    Create Home.php

---
+++ Home.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <link rel="stylesheet" type="text/css" href="\css\style.css"/>
+    <title>Welcome to CSIS 410</title>
+</head>
+<body>
+    <!--Menu-->
+    <?php 
+        include("../../menu.php");
+    ?>
+    
+    <!--Review of Hostica-->
+    <p>     Welcome to Assignment 1. Website hosting is the bread and butter of maintaining a website, choosing a reliable and trustworthy host is vital. This website is hosted by Google Domains and provides many features and services that are useful. Offering not only domain name purchasing, and hosting services, but also, SQL and PHP capabilities, and a very simple to use interface.</p>
+    
+    <p>     There are disadvantages to Google Domains. Google Domains does not have wide coverage and is not available to many countries. Despite how easy to use Google domains interface is for those who want to avoid having to do the hard HTML coding it is prohibitive when attempting to create a website from scratch through the use of coding. Finally the service is more expensive than many other options on the market, especially considering many services offer up to a year of free hosting. Regardless of the flaws the hosting platform is worth recommending to those with little interest in running a website without the assistance of a UI that does most of the work for you.
+    </p>
+    
+    <p>If you would like to view the PHP info of this assignment, please click <a href="phpinfo.php">here</a>.
+    </p>
+   
+    <!--Footer-->
+    <?php
+        include("../../footer.php");
+    ?>
+</body>
+</html>
