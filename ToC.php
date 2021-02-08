71d31db40303705a1874c8b33729bcc448b92bab
R1b0flav1n <crdickson97@gmail.com>
2021-02-08 18:17:05 -0500

    Update ToC.php

--- ToC.php
+++ ToC.php
@@ -3,8 +3,7 @@
 
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
-    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
-    <link rel="stylesheet" type="text/css" href="\css\style.css"/>
+
 </head>
 <body>
     <div class="submenu">
