0664fee049ad73d7403035ea94cb9f3f02f1bdae
R1b0flav1n <crdickson97@gmail.com>
2021-02-08 17:49:10 -0500

    Update Home.php

--- Home.php
+++ Home.php
@@ -14,12 +14,12 @@
     ?>
     
     <!--Review of Hostica-->
-    <p>     Welcome to Assignment 1. Website hosting is the bread and butter of maintaining a website, choosing a reliable and trustworthy host is vital. This website is hosted by Google Domains and provides many features and services that are useful. Offering not only domain name purchasing, and hosting services, but also, SQL and PHP capabilities, and a very simple to use interface.</p>
+    <p>     Welcome to CIS project 1. Website hosting is the bread and butter of maintaining a website, choosing a reliable and trustworthy host is vital. This website is hosted by Github and provides many features and services that are useful. Offering ease of collaboration, back ups, and immense documentation to help resolve any problems.</p>
     
-    <p>     There are disadvantages to Google Domains. Google Domains does not have wide coverage and is not available to many countries. Despite how easy to use Google domains interface is for those who want to avoid having to do the hard HTML coding it is prohibitive when attempting to create a website from scratch through the use of coding. Finally the service is more expensive than many other options on the market, especially considering many services offer up to a year of free hosting. Regardless of the flaws the hosting platform is worth recommending to those with little interest in running a website without the assistance of a UI that does most of the work for you.
+    <p>     There are unfortunatley some disadvatages to using Github. Many of Git hubs features are locked behind payment creating a frustrating experience when issues can only be resolved through paid features. Additionally Github is not known for having adequate security and is therefore riskier than other possible services. Regardless of the flaws the hosting platform is worth recommending to those with little interest in running a website without the assistance of a UI that does most of the work for you that is also mostly free.
     </p>
     
-    <p>If you would like to view the PHP info of this assignment, please click <a href="phpinfo.php">here</a>.
+    <p>In order to view the PHP info of this assignment, please click <a href="phpinfo.php">here</a>.
     </p>
    
     <!--Footer-->
