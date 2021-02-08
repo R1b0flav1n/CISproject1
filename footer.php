72dc785b0d35383a6c69b777bf47afa667dfcb2e
R1b0flav1n <crdickson97@gmail.com>
2021-02-08 17:57:21 -0500

    Update footer.php

--- footer.php
+++ footer.php
@@ -5,13 +5,13 @@
 <head>
     <meta http-equiv="content-type" content="text/html; charset=utf-8" />
     <link rel="stylesheet" type="text/css" href="\css\style.css"/>
-    <title>Assignment 1</title>
+    <title>CISproject1</title>
 </head>
 <body>
     <div id="footer">
         <a href="https://validator.w3.org/check?uri=referer"><img src="http://www.w3.org/Icons/valid-xhtml10" alt="Valid XHTML 1.0!" height="31" width="88" /></a>
         <?php
-        filemtime("index.php");
+        filemtime("home.php");
         echo "<br />";
         echo "Last modified: ".date("F d Y H:i:s.", filemtime("index.php"));
         if (!isset($_SESSION['user'])) {
