a2653cca6aadd41bd9c03d4e0b95b64c70afca1f
R1b0flav1n <crdickson97@gmail.com>
2021-02-08 18:37:36 -0500

    Update index.php

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
         include("../../ToC.php");
     ?>
     
-    <!--Review of Hostica-->
+    <!--Review-->
     <p>     Welcome to CIS project 1. Website hosting is the bread and butter of maintaining a website, choosing a reliable and trustworthy host is vital. This website is hosted by Github and provides many features and services that are useful. Offering ease of collaboration, back ups, and immense documentation to help resolve any problems.</p>
     
     <p>     There are unfortunatley some disadvatages to using Github. Many of Git hubs features are locked behind payment creating a frustrating experience when issues can only be resolved through paid features. Additionally Github is not known for having adequate security and is therefore riskier than other possible services. Regardless of the flaws the hosting platform is worth recommending to those with little interest in running a website without the assistance of a UI that does most of the work for you that is also mostly free.
